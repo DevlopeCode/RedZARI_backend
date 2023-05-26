3b027040130d67a102e3710660b0545e208afc35
prabhat <pranhat@pranhats-MacBook-Pro.local>
2023-05-26 13:34:18 +0530

    Add product

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const connectToMongo = () => {
   mongoose
-    .connect("mongodb://localhost:27017", {
+    .connect("mongodb+srv://redzhari:wE5m4qXOSQjOaW3t@cluster0.50gcuib.mongodb.net/test", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
