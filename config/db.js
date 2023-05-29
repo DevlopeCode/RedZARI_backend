3885491c74b966d6c666d06bf87998fb095c9f45
prabhat <pranhat@pranhats-MacBook-Pro.local>
2023-05-29 15:09:52 +0530

    add code

--- config/db.js
+++ config/db.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+// const connectToMongo = () => {
+//   mongoose
+//     .connect("mongodb+srv://redzhari:wE5m4qXOSQjOaW3t@cluster0.50gcuib.mongodb.net/test", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//     .then(() => {
+//       console.log("Connected to MongoDb Atlas!");
+//     })
+//     .catch((err) => {
+//       console.log("ERROR:", err.message);
+//     });
+// };
 const connectToMongo = () => {
   mongoose
-    .connect("mongodb+srv://redzhari:wE5m4qXOSQjOaW3t@cluster0.50gcuib.mongodb.net/test", {
+    .connect("mongodb+srv://redzarifashion:pDr48eaQI97v4Wr9@cluster0.cneoo7b.mongodb.net/redzari", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
@@ -15,5 +28,3 @@ const connectToMongo = () => {
     });
 };
 module.exports = connectToMongo;
-// redzhari
-// wE5m4qXOSQjOaW3t
